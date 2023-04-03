61ebc09ae9db66d6ba0a79bf57e3a511d9cef309
Mirkoiandoni <mirko.96@live.it>
2023-04-03 16:59:57 +0200

    prime prove basi js

---
+++ m1/s2/s2/g1/esercizio.js
@@ -0,0 +1,41 @@
+var anni= prompt( "Madfox_96 ,quanti anni ha ?" );
+ var tifo= prompt ("che squadra tifa Madfox_96 ? esempio inter" ) ;
+  var tempo= prompt ("da quanti mesi Madfox_96 è su la piattaforma?");
+  
+
+
+    
+
+if (anni ==27){
+    document.write("Giusto");
+}
+
+else {document.write("NON è GIUSTO ");
+}
+
+
+if (tifo =="Roma"){
+document.write("Bravo!");
+}
+else {document.write("hai sbagliato");
+}
+
+
+if (tempo =="9"){
+document.write("molto bene sei Degno!");
+}
+else {document.write("male male ! ");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+   
