5bcef3cfbaa2ba86890275805a5730c58e89d019
Mirko iandoni <mirko.96@live.it>
2023-05-29 17:24:22 +0200

    esercizio di lunedi

--- m3/s2/g1/progetto1/src/app/app.module.ts
+++ m3/s2/g1/progetto1/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    MainComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
