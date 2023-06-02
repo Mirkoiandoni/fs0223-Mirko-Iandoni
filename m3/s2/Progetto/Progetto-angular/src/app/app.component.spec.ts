e58f747cd3477ba0fe9aa8ee8b83180d3ccf9260
Mirko iandoni <mirko.96@live.it>
2023-06-02 09:53:49 +0200

    l'inizio

--- m3/s2/Progetto/Progetto-angular/src/app/app.component.spec.ts
+++ m3/s2/Progetto/Progetto-angular/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Progetto-angular'`, () => {
+  it(`should have as title 'progetto-angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Progetto-angular');
+    expect(app.title).toEqual('progetto-angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Progetto-angular app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('progetto-angular app is running!');
   });
 });
