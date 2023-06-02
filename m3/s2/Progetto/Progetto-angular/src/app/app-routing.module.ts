5bf8d2549b4f80695a5b9f82a4ed6c5c89a8dc16
Mirko iandoni <mirko.96@live.it>
2023-06-02 11:38:25 +0200

    aggiornamenti

--- m3/s2/Progetto/Progetto-angular/src/app/app-routing.module.ts
+++ m3/s2/Progetto/Progetto-angular/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CompletedComponent } from './completed/completed.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { TodoComponent } from './todo/todo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'todo', component: TodoComponent },
+  { path: '', component: HomepageComponent },
+  { path: 'completed', component: CompletedComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- m3/s2/Progetto/Progetto-angular/src/app/app.module.ts
+++ m3/s2/Progetto/Progetto-angular/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { TodoComponent } from './todo/todo.component';
+import { CompletedComponent } from './completed/completed.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomepageComponent,
+    TodoComponent,
+    CompletedComponent
   ],
   imports: [
     BrowserModule,
